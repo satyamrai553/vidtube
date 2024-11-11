129dc87eaecfb2caf14c078aa7b2a21d27ced2cb
Satyam Rai <satyamrai553@gamil.com>
2024-11-11 12:57:09 +0530

    git initialize

---
+++ backend/src/db/index.js
@@ -0,0 +1,18 @@
+import { DB_NAME } from "../constants.js";
+import mongoose from "mongoose";
+
+
+
+const connectDB = async ()=>{
+    try {
+        const response = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connecred ! DB host: ${response.connection.host}`);
+        
+    } catch (error) {
+        console.log("MongoDB connection error: ", error)
+        process.exit(1);
+    }
+}
+
+
+export default connectDB;
